4ad7d19986a83d9287f3f924a62ec243f1fe38fa
blankdots <stefan@blankdots.com>
2017-11-08 13:21:36 +0200

    ATTX-project/indexing-service#3 REST API data/index endpoint

--- src/esindex/api/indexing.py
+++ src/esindex/api/indexing.py
@@ -2,17 +2,21 @@ import json
 import falcon
 from esindex.utils.logs import app_logger
 from esindex.applib.elastic_index import ElasticIndex
+from esindex.schemas import load_schema
+from esindex.utils.validate import validate
+from esindex.applib.construct_message import add_message
 
 
 class IndexClass(object):
     """Create Indexing class for API."""
 
+    @validate(load_schema('index'))
     def on_post(self, req, resp, parsed):
         """Respond on GET request to index endpoint."""
-        resp.data = json.dumps(["data"], indent=1, sort_keys=True)
+        resp.data = add_message(parsed)
         resp.content_type = 'application/json'
         resp.status = falcon.HTTP_202
-        app_logger.info('Creating/POST data to index.')
+        app_logger.info('Index/POST data with specified aliases.')
 
 
 class AliasesList(object):
--- src/esindex/app.py
+++ src/esindex/app.py
@@ -1,7 +1,7 @@
 import falcon
 from esindex.utils.logs import app_logger
 from esindex.api.healthcheck import HealthCheck
-from esindex.api.indexing import AliasesList
+from esindex.api.indexing import AliasesList, IndexClass
 
 api_version = "0.2"  # TO DO: Figure out a better way to do versioning
 
@@ -13,6 +13,8 @@ def init_api():
     indexservice.add_route('/health', HealthCheck())
 
     indexservice.add_route('/%s/alias/list' % (api_version), AliasesList())
+
+    indexservice.add_route('/%s/data/index' % (api_version), IndexClass())
 
     app_logger.info('IndexService REST API is running.')
     return indexservice
